90a9b37d0ed5fc815a65ebabf15872d1a155c2e9
red1484 <torticemir@gmail.com>
2024-12-27 23:19:49 -0700

    add a framerate clock

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
                 return
         screen.fill(color=(0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
