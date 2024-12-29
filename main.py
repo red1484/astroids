59e068107ef6b2c9b5298db81452ced3e3face59
red1484 <torticemir@gmail.com>
2024-12-28 18:45:31 -0700

    add a player spaceship

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
