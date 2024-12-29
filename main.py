9d093f95f04d58b6ef3660033611e242363314b6
red1484 <torticemir@gmail.com>
2024-12-29 09:36:33 -0700

    move player objects to groups in main

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -20,8 +23,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
